0c7d0c01124f7f0144dbb15fe166df7c1957aef4
minkaizen <martin.g.cao@gmail.com>
2025-01-18 08:28:52 +1100

    feat(commands): add skim-up and skim-down commands

--- main.ts
+++ main.ts
@@ -2,10 +2,24 @@ import { Plugin } from "obsidian";
 
 export default class SkimPlugin extends Plugin {
   async onload() {
-    console.log("Plugin loaded");
-  }
-
-  onunload() {
-    console.log("Plugin unloaded");
+    this.addCommand({
+      id: "skim-down",
+      name: "Skim down",
+      editorCallback: (editor) => {
+        const currentCursor = editor.getCursor();
+        const maxLine = editor.lineCount() - 1;
+        const newLine = Math.min(currentCursor.line + 7, maxLine);
+        editor.setCursor({ line: newLine, ch: currentCursor.ch });
+      },
+    });
+    this.addCommand({
+      id: "skim-up",
+      name: "Skim up",
+      editorCallback: (editor) => {
+        const currentCursor = editor.getCursor();
+        const newLine = Math.max(0, currentCursor.line - 7);
+        editor.setCursor({ line: newLine, ch: currentCursor.ch });
+      },
+    });
   }
 }
